3e8c3ece171faad142bcdd2ddbc5ca7595832565
Wilson <sabdiel.rivera@gmail.com>
2026-06-04 22:03:18 -0400

    Fix About Me Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   year = new Date().getFullYear();
+  showHero = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe((e: NavigationEnd) => {
+        this.showHero = e.urlAfterRedirects === '/clients';
+      });
+  }
 }
